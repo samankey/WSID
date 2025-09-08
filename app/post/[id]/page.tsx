3c3ac801c2aeead64f8f648d24e0901caa28dcb1
samankey <samankeyc@gmail.com>
2025-09-08 23:27:55 +0900

    task: add await on params

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,10 +1,15 @@
 import { PostDetail } from "@/components/features/post/PostDetail";
 
-export default function PostDetailPage({ params }: { params: { id: string } }) {
+export default async function PostDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   // 서버에서 id만 받고, 실제 데이터는 우선 mock으로
+  const { id } = await params;
   return (
     <main className="py-6">
-      <PostDetail postId={params.id} />
+      <PostDetail postId={id} />
     </main>
   );
 }
